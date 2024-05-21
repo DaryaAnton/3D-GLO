253473059f617ca947c88049ecb87b28e1970185
Darya Anton <foxik7477@gmail.com>
2024-05-21 08:39:23 +0500

    промежуточное сохранение

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,13 +2,11 @@ const modalModule = () => {
     const buttons = document.querySelectorAll('.popup-btn');
     const modal = document.querySelector('.popup');
     const closeBtn = modal.querySelector('.popup-close');
-    let active = false;
-    
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             modal.style.display = 'block';
-            if (active === false && window.screen.width > 768) {
+            if (window.screen.width > 768) {
                 animationModal(modal, 1500);
             }
         });
@@ -18,18 +16,21 @@ const modalModule = () => {
     });
 
     function animationModal(elem, duration) {
-        var start = null;
-        function step(mark) {
-          if (!start) start = mark;
-          var progress = mark - start;
-          elem.style.opacity = Math.min(progress / duration, 1);
-          if (progress < duration) {
+        let start;
+        
+        function step(timestamp) {
+            if (!start) {
+                start = timestamp;
+            }; 
+            const opacity = (timestamp - start) / duration;
+            elem.style.opacity = opacity;
+            if (opacity >= 1) {
+                return;
+            };
             requestAnimationFrame(step);
-          };
         };
         requestAnimationFrame(step);
-    };
+    }
 };
 
 export default modalModule;
-//что происходит вообьще?!
